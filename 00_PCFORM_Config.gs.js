ab1e153eab1e454b37615cbb798f4b7cef6b30c9
primecarediagnosticsuppliesadm-cmyk <primecarediagnosticsuppliesadm@gmail.com>
2026-03-13 00:48:59 +0000

    Sync Apps Script changes including PCFORM config

---
+++ 00_PCFORM_Config.gs.js
@@ -0,0 +1,23 @@
+/************************************************************
+ * 00_PCFORM_Config.gs
+ * PrimeCare Form constants
+ ************************************************************/
+
+const PCFORM = Object.freeze({
+
+  RAW_SHEET: "Form_Responses_Raw",
+
+  ORDERS_SHEET: "Orders",
+  ORDER_LINES_SHEET: "Order_Lines",
+
+  PRODUCT_MASTER_SHEET: "Product_Master",
+
+  INVOICE_REGISTER_SHEET: "Invoice_Register",
+
+  ERP_EXPORT_SHEET: "ERP_Export",
+
+  SF_EXPORT_SHEET: "Salesforce_Export",
+
+  SETTINGS_SHEET: "Settings"
+
+});
